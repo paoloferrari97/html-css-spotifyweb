2180ec6c35da189d5d1ebc60fce3213d525a47d7
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-04-21 13:51:01 +0200

    Uso JavaScript sul primo titolo (recently played)

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+//writeln
+document.writeln("Prova JavaScript 1");
+
+//alert
+alert("Prova messaggio di allerta JavaScript 2");
+
+//getElementById
+document.getElementById("titolo_uno").innerHTML = "Prova titolo JavaScript 3";
